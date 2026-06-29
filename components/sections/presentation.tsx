2a5341af7f29251b9f6e0cab645c95f5202d4738
nemnem202 <naim.elhabbas@gmail.com>
2026-06-29 09:57:38 +0200

    feat: added grid and items-positions on presentation

--- components/sections/presentation.tsx
+++ components/sections/presentation.tsx
@@ -5,12 +5,23 @@ export default function PresentationSection() {
   return (
     <Section id="presentation">
       <SectionContent className="flex gap-12">
-        <></>
-        {/* <p>
-          Velit cupidatat commodo adipisicing est consequat mollit veniam mollit deserunt ad
-          consequat cillum labore dolor. Tempor ullamco in aute deserunt irure proident laboris
-          nostrud magna officia. Consectetur cillum aute elit mollit. Et enim eu ad pariatur.
-        </p> */}
+        <div className="grid grid-cols-15 grid-rows-10 size-full">
+          <div className="col-start-6 col-end-9 row-start-4 row-end-8 size-full flex items-center justify-center">
+            <div className="bg-primary rounded-full w-full aspect-square"></div>
+          </div>
+          <div className="col-start-1 col-end-6 row-start-1 row-end-4 size-full flex items-center justify-center">
+            <div className="bg-primary size-full rounded-lg"></div>
+          </div>
+          <div className="col-start-2 col-end-5 row-start-7 row-end-10 size-full flex items-center justify-center">
+            <div className="bg-primary size-full rounded-lg"></div>
+          </div>
+          <div className="col-start-10 col-end-16 row-start-2 row-end-5 size-full flex items-center justify-center">
+            <div className="bg-primary size-full rounded-lg"></div>
+          </div>
+          <div className="col-start-9 col-end-16 row-start-8 row-end-11 size-full flex items-center justify-center">
+            <div className="bg-primary size-full rounded-lg"></div>
+          </div>
+        </div>
       </SectionContent>
       <Title>Me</Title>
     </Section>
